7bb611ac19958a8117ad77c34d2f0b2314c790e8
Vanya Drozdov <ivan.drozdov880@gmail.com>
2024-03-11 09:03:07 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,12 @@
 // pixabay-api.js
-export async function fetchImages(query) {
+export function fetchImages(query) {
   const API_KEY = '42796479-140a0b0d57e5aafe2bfea6b1d';
   const BASE_URL = 'https://pixabay.com/api/';
   const IMAGE_TYPE = 'photo';
   const ORIENTATION = 'horizontal';
   const SAFESEARCH = 'true';
 
-  const response = await fetch(
+  const response = fetch(
     `${BASE_URL}?key=${API_KEY}&q=${query}&image_type=${IMAGE_TYPE}&orientation=${ORIENTATION}&safesearch=${SAFESEARCH}`
   );
 
@@ -14,7 +14,7 @@ export async function fetchImages(query) {
     throw new Error('Failed to fetch images');
   }
 
-  const data = await response.json();
+  const data = response.json();
   const images = data.hits;
 
   if (!Array.isArray(images) || images.length === 0) {
